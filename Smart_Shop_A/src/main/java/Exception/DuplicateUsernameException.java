6d5ddc60b350a61d44f89f53d6cb411dd09a5413
Vikas1267 <159723607+Vikas1267@users.noreply.github.com>
2026-08-18 00:43:39 +0530

    Add files via upload

---
+++ Smart_Shop_A/src/main/java/Exception/DuplicateUsernameException.java
@@ -0,0 +1,8 @@
+package Exception;
+
+public class DuplicateUsernameException extends Exception {
+
+	public DuplicateUsernameException(String message) {
+		super(message);
+	}
+}
---
+++ Smart_Shop_A/src/main/java/Exception/InvalidLoginException.java
@@ -0,0 +1,8 @@
+package Exception;
+
+public class InvalidLoginException extends Exception  {
+
+	public InvalidLoginException(String message) {
+		super(message);
+	}
+}
